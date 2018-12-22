25d5ff735f01cc4c35cdf7a4a8f490b66aef3502
Francis Gulotta <wizard@roborooter.com>
2018-12-22 10:29:07 -0500

    have webpack add the shebang

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const {BannerPlugin} = require('webpack')
 
 module.exports = {
   entry: './lib/index.js',
@@ -8,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
   },
+  plugins: [
+    new BannerPlugin({banner: '#!/usr/bin/env node', raw: true})
+  ],
   externals: {
     'pty.js': 'pty.js',
     'term.js': 'term.js',
